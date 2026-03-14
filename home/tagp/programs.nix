e322ee2867b949b108b0d91eb810429648e0290f
Thomas Petersson <thomas@petersson.priv.no>
2026-03-14 17:40:37 +0100

    nix depth shell

--- home/tagp/programs.nix
+++ home/tagp/programs.nix
@@ -24,4 +24,17 @@
 		bash-language-server
 		marksman
   ];
+
+	programs.bash.initExtra = ''
+    export NIX_SHELL_DEPTH=$(( ''${NIX_SHELL_DEPTH:-0} + 1 ))
+
+    _nix_shell_indicator() {
+      if [[ -n "$IN_NIX_SHELL" ]]; then
+        local label="''${name:-''${IN_NIX_SHELL}}"
+        echo "(nix:''${NIX_SHELL_DEPTH}:''${label}) "
+      fi
+    }
+
+    PS1='$(_nix_shell_indicator)'"$PS1"
+  '';
 }
